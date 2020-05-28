01a29ce0c133c908302f170d6e73edc713ac5060
rtanubra <rdtanubrata@gmail.com>
2020-05-28 19:51:49 -0400

    working

--- pmMain/admin.py
+++ pmMain/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Owner, Prop_Name, Reserve, Room_Info
-
+from .models import Owner, Prop_Name, Reserve, Room_Info,  Expenses, Earnings ,Room_Transactions
 # Register your models here.
 admin.site.register(Owner)
 admin.site.register(Prop_Name)
 admin.site.register(Reserve)
 admin.site.register(Room_Info)
+admin.site.register(Expenses)
+admin.site.register(Earnings)
+admin.site.register(Room_Transactions)
+
